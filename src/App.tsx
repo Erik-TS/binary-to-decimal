5f5ba3980a139906186e60a394adebf9cdf3ff02
Erik-TS <crocruk@outlook.com>
2023-10-06 00:49:11 -0300

    Create converter function and UI prototype.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [decimal, setDecimal] = useState(0)
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <h1>Binary to Decimal</h1>
+      <textarea onChange={(e) => {
+        const userInput: string = e.target.value
+
+        setDecimal(binaryToDecimal(userInput))
+      }} cols={30} rows={10}></textarea>
+      <h1>{decimal}</h1>
     </>
   )
+}
+
+function binaryToDecimal(binary: string): number {
+  const decimal = parseInt(binary, 2)
+
+  return decimal
 }
 
 export default App
