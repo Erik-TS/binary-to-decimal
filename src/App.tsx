3a5aeb923547e3fe2848a57b687832b9772cc609
Erik-TS <crocruk@outlook.com>
2023-10-08 19:19:57 -0300

    Improve UI structure and style. Write function 'decimalToBinary'.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  const [decimal, setDecimal] = useState(0)
-
   return (
-    <>
+    <div className={"main-container"}>
+      <Background />
       <h1>Binary to Decimal</h1>
-      <textarea onChange={(e) => {
-        const userInput: string = e.target.value
+      <div className={"convertion-area"}>
+        <textarea
+          className={"binary-area"}
+          onChange={(e) => {
+            const userInput: string = e.target.value
+            const d = (document.querySelector(".decimal-area") as HTMLTextAreaElement)
 
-        setDecimal(binaryToDecimal(userInput))
-      }} cols={30} rows={10}></textarea>
-      <h1>{decimal}</h1>
-    </>
+            d.textContent = binaryToDecimal(userInput).toString()
+          }} cols={30} rows={10}>
+        </textarea>
+        <textarea className={'decimal-area'} readOnly={true} cols={30} rows={10}>
+        </textarea>
+      </div>
+    </div>
+  )
+}
+
+function Background(){
+  return(
+    <div className={'page-background'}>
+      <div className={"binary-background"}></div>
+      <div className={"decimal-background"}></div>
+    </div>
   )
 }
 
@@ -22,6 +36,40 @@ function binaryToDecimal(binary: string): number {
   const decimal = parseInt(binary, 2)
 
   return decimal
+}
+
+function decimalToBinary(decimal: number): string {
+  const logArr: Array<number> = []
+
+  while (decimal > 0) {
+    const logaritmo = Math.log2(decimal)
+    logArr.push(Math.floor(logaritmo))
+    decimal -= (2 ** logArr[logArr.length - 1])
+  }
+
+  function generateBinary(logArr: Array<number>): string {
+    let binary = ""
+
+    for (let i = 0; i < logArr[0]; i++) {
+      const len = binary.length + 1
+
+      for (const log of logArr) {
+        if (log === i) binary = binary.padStart(len, "1")
+        else if (log < i) break
+      }
+
+      if (logArr.includes(i)) {
+        binary = binary.padStart(len, "1")
+      }
+      else {
+        binary = binary.padStart(len, "0")
+      }
+    }
+
+    return binary
+  }
+
+  return generateBinary(logArr)
 }
 
 export default App
---
+++ src/Info.tsx
@@ -0,0 +1,11 @@
+export default function Info() {
+    return (
+        <div className={"info-container"}>
+            <h2>What are binary numbers?</h2>
+            <p>We are used to work with decimal numbers, like: 7, 15, 921.
+                Binary numbers are similar, but represented differently.
+                Instead of the usual digits (0, 1, 2, 3, 4, 5, 6, 7, 8, 9), binary numbers use only two, 0 and 1.
+            </p>
+        </div>
+    )
+}
